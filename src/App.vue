6cde98215246727b6d5fc7801a16b912e64e92ad
EdwardKerckhof <edwardkerckhof@hotmail.com>
2025-02-07 19:54:30 +0100

    feat(setup): add tailwind

--- src/App.vue
+++ src/App.vue
@@ -15,32 +15,27 @@
 
 <style scoped lang="scss">
     header {
-        line-height: 1.5;
-        max-height: 100vh;
-    }
+        nav {
+            width: 100%;
+            font-size: 14px;
+            text-align: center;
+            margin-top: 2rem;
 
-    nav {
-        width: 100%;
-        font-size: 12px;
-        text-align: center;
-        margin-top: 2rem;
-    }
+            a {
+                display: inline-block;
+                padding: 0 1rem;
+                border-left: 1px solid gray;
+                color: inherit;
+                transition: color 0.3s ease;
 
-    nav a.router-link-exact-active {
-        color: var(--color-text);
-    }
+                &.router-link-exact-active {
+                    color: hsla(160, 100%, 37%, 1);
+                }
 
-    nav a.router-link-exact-active:hover {
-        background-color: transparent;
-    }
-
-    nav a {
-        display: inline-block;
-        padding: 0 1rem;
-        border-left: 1px solid var(--color-border);
-    }
-
-    nav a:first-of-type {
-        border: 0;
+                &:first-of-type {
+                    border: 0;
+                }
+            }
+        }
     }
 </style>
